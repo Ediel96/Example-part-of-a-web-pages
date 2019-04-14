f373078ce0bde1cad576fbd5ef6b1d7735f3ed1c
Ediel96 <h.amiltone.diel@gmail.com>
2019-04-14 16:07:38 -0500

    +

---
+++ 01-login/scripts/scripts.js
@@ -0,0 +1,6 @@
+
+ function validar()
+{
+    var user = document.getElementById(user).value;
+    //https://www.youtube.com/watch?v=qOXxulKbJLk
+}
--- login/scripts/scripts.js
+++
@@ -1,6 +0,0 @@
-
- function validar()
-{
-    var user = document.getElementById(user).value;
-    //https://www.youtube.com/watch?v=qOXxulKbJLk
-}
